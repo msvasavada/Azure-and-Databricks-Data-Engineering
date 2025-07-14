58ada2a6a0402f47df715d4a2cbaa1a33f0b0a63
Meghana Vasavada <105937565+msvasavada@users.noreply.github.com>
2025-07-14 19:09:13 -0400

    Add files via upload

--- Databricks_Data_Engineering/DE_Labs/Lab Job.py
+++ Databricks_Data_Engineering/DE_Labs/Lab Job.py
@@ -1,9 +1,10 @@
 # Databricks notebook source
-# MAGIC %md-sandbox
+# MAGIC %md
 # MAGIC
 # MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
-# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning">
 # MAGIC </div>
+# MAGIC
 
 # COMMAND ----------
 
@@ -25,15 +26,18 @@ DA = DBAcademyHelper(course_config=course_config,
 
 # COMMAND ----------
 
-query = f"UPDATE {DA.catalog_name}.job_lab_schema.sample_data SET promotion_type = '4' WHERE promotion_type = '2'"
+query = f"UPDATE {DA.catalog_name}.job_lab_schema.promotion_data SET promotion_type = '4' WHERE promotion_type = '2'"
 
 # Execute the query and store the result
 query_result = spark.sql(query)
 
 # COMMAND ----------
 
-# MAGIC %md-sandbox
-# MAGIC &copy; 2023 Databricks, Inc. All rights reserved.<br/>
-# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
-# MAGIC <br/>
-# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="https://help.databricks.com/">Support</a>
+# MAGIC %md
+# MAGIC
+# MAGIC &copy; 2024 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the 
+# MAGIC <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/><a href="https://databricks.com/privacy-policy">Privacy Policy</a> | 
+# MAGIC <a href="https://databricks.com/terms-of-use">Terms of Use</a> | 
+# MAGIC <a href="https://help.databricks.com/">Support</a>
